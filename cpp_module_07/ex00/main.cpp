e5c6e9b73f08e521f815149e9066468473a4b583
mingkang <mingkang@student.42seoul.kr>
2023-09-21 17:24:08 +0900

    cpp_module07-ex00 main내용 추가

--- cpp_module_07/ex00/main.cpp
+++ cpp_module_07/ex00/main.cpp
@@ -6,7 +6,9 @@ int main( void ) {
 
 	int a = 2;
 	int b = 3;
-
+	
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "swap( a, b )" << std::endl;
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
@@ -15,10 +17,12 @@ int main( void ) {
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "swap( c, d )" << std::endl;
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-	
+
 	return (0);
 }
