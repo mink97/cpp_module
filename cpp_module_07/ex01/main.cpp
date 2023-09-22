815fe5c8781078c0d67c2b7ffe1b10ceef316f2b
mink97 <kmg8146@naver.com>
2023-09-22 15:00:45 +0900

    edit main

--- cpp_module_07/ex01/main.cpp
+++ cpp_module_07/ex01/main.cpp
@@ -5,9 +5,9 @@ int main()
 {
 	int arr1[10] = {1 , 23, 35, 2346, 2354, 234, 45, 6785, 675, 234};
 
-	::iter(arr1, 10, ::ft_print);
+	::iter(arr1, 10, ::ft_print<int>);
 
 	std::string strArr[5] = {"as", " ming", "kang", "min", "gwan"};
-	::iter(strArr, 5, ::ft_print);
+	::iter(strArr, 5, ::ft_print<std::string>);
 
 }
